b38ce0b1fa0632e5973cefd9307f46f291a1df32
vicious721 <du-du7@hotmail.com>
2018-11-09 23:36:43 -0200

    Atualização footer

--- src/App.vue
+++ src/App.vue
@@ -23,20 +23,24 @@
       </v-layout>
       <router-view :style="{ backgroundColor: this.$vuetify.theme.accent }"></router-view>
         <v-footer dark height="auto">
-          <v-card flat tile class="blue darken-4 white--text text-xs-center">
-            <v-card-text>
-              <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon >
-                <v-icon size="24px">{{ icon }}</v-icon>
-              </v-btn>
-            </v-card-text>
-            <v-card-text class="white--text pt-0">
-              Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-text class="white--text">
-              &copy;2018 — <strong>TechBoys</strong>
-            </v-card-text>
-          </v-card>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <v-card flat tile class="blue darken-4 white--text text-xs-center">
+                <v-card-text>
+                  <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon >
+                    <v-icon size="24px">{{ icon }}</v-icon>
+                  </v-btn>
+                </v-card-text>
+                <v-card-text class="white--text pt-0">
+                  Desenvolvido por: Fudidos Crew
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-text class="white--text">
+                  &copy;2018 — <strong>TechBoys</strong>
+                </v-card-text>
+              </v-card>              
+            </v-flex>            
+          </v-layout>
         </v-footer>
     </v-container>
   </v-app>
@@ -164,8 +168,6 @@ export default {
       icons: [
         'fab fa-facebook',
         'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
         'fab fa-instagram'
       ]
     };
