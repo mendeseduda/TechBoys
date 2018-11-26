1962db379307a2c58d5b92b6c33e689ae83413fc
vicious721 <du-du7@hotmail.com>
2018-11-26 01:27:43 -0200

    Finalização protótipo

---
+++ src/components/About.vue
@@ -0,0 +1,126 @@
+<template>
+    <v-app>
+        <v-container grid-list-xs>
+            <v-layout column align-center wrap justify-center>
+                <v-flex ma-3>
+                    <v-img width="450px" src="../../public/assets/images/arcade-font.png"></v-img>
+                </v-flex>
+                <div style="max-width: 600px">
+                    <v-layout justify-center row wrap mt-3 class="round-border">
+                        <div v-for="member in members" :key="member" class="effect">
+                            <v-img position="0 center" width="150px" height="150px" :src="'../../public/assets/images/techboys/' + member.name + '.jpg'">
+                                <div class="overlay-effect">
+                                    <div class="overlay-item white--text subheading break-word">
+                                        <div>{{member.name}}</div>
+                                        <div class="description">
+                                            {{member.description}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </v-img>
+                        </div>
+                    </v-layout>
+                </div>
+            </v-layout>
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      members: [
+        {
+          name: "John",
+          description: "Estranho"
+        },
+        {
+          name: "Maia",
+          description: "Matemático Padrão"
+        },
+        {
+          name: "Ferreira",
+          description: "Fede"
+        },
+        {
+          name: "Eduardo",
+          description: "Deus do Front-End"
+        },
+        {
+          name: "Juliana",
+          description: "Um Amor"
+        },
+        {
+          name: "Matheus",
+          description: "Namorado da Carine"
+        },
+        {
+          name: "Motta",
+          description: "Menino Engenheiro"
+        },
+        {
+          name: "Pepe",
+          description: "Rico"
+        },
+        {
+          name: "Roberto",
+          description: "Corno"
+        },
+        {
+          name: "Hugo",
+          description: "Talentoso"
+        },
+        {
+          name: "Mochi",
+          description: "Desenvolvedor Sênior"
+        },
+        {
+          name: "Livia",
+          description: "GOD"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.effect:hover, .effect:hover > *{
+    transform: scale(1.01);
+    z-index: 2;
+    transition: all .2s;
+    box-shadow: 0 0 30px #FFA000;
+}
+
+.overlay-effect{
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: center;
+}
+
+.overlay-item{
+    background-color: rgb(0, 0, 0, 0.5);
+    width: 100%;
+    transform: translateY(100px);
+    transition: all .2s;
+}
+
+.effect:hover .overlay-item{
+    transform: translateY(0);
+} 
+
+.break-word{
+    word-break: break-word;
+}
+
+.description{
+    color: #ffffffbf;
+    font-size: 14px;
+}
+
+</style>
+
--- src/components/Podcasts.vue
+++ src/components/Podcasts.vue
@@ -4,22 +4,23 @@
           <v-layout row wrap justify-center>
               <v-flex ma-2 v-for="episode in podcasts" :key="episode.id" xs12 sm6 md4 lg3 xl2>
                   <v-card>
-                    <v-img :src="episode.image" height="200px" @mouseover="episode.show = true" @mouseout="episode.show = false">
+                    <v-img :src="'../../public/assets/images/podcasts/podcast'+ episode.id +'.jpg'" height="200px" @mouseover="episode.show = true" @mouseout="episode.show = false">
                         <v-container fill-height fluid pa-0>
                             <v-layout fill-height row justify-start class="container-overlay">
-                                <v-flex pa-2 xs12 flexbox text-xs-left>
+                                <v-flex v-if="!episode.show" pa-2 xs12 flexbox text-xs-left>
                                     <span class="headline white--text" v-text="episode.title"></span>
                                 </v-flex>
-                                <v-flex v-if="episode.show" fill-height fill-width class="overlay">
-                                    <v-icon color="white" style="font-size: 42px;">fas fa-play</v-icon>
+                                <v-flex v-if="episode.show" fill-height fill-width class="overlay" @click="playAudio('../../public/assets/songs/pod'+ episode.id + '.mp3')">
+                                    <v-icon color="white" style="font-size: 42px;">{{ !playing ?  'fas fa-play' : 'fas fa-pause'}}</v-icon>
                                 </v-flex>
                             </v-layout>
                         </v-container>
                     </v-img>
                     <v-card-actions>
+                        <span class="subheading">Monistorinhas: {{episode.id}}</span>
                         <v-spacer></v-spacer>
-                        <v-btn icon>
-                            <v-icon>favorite</v-icon>
+                        <v-btn icon @click="episode.favorite = !episode.favorite">
+                            <v-icon v-bind:class="{active: episode.favorite}">favorite</v-icon>
                         </v-btn>
                         <v-btn icon>
                             <v-icon>share</v-icon>
@@ -37,44 +38,53 @@ export default {
   name: "Podcasts",
   data() {
     return {
+      audio: new Audio(),
+      playing: false,
       podcasts: [
         {
-          id: 1,
+          id: 5,
           title: "Esportes",
-          image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-          src: "",
-          show: false
+          show: false,
+          favorite: false
         },
         {
-          id: 2,
+          id: 4,
           title: "Fim do Mundo",
-          image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-          src: "",
-          show: false
+          show: false,
+          favorite: false
         },
         {
           id: 3,
           title: "Pirataria",
-          image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-          src: "",
-          show: false
+          show: false,
+          favorite: false
         },
         {
-          id: 4,
-          title: "Pirataria",
-          image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-          src: "",
-          show: false
+          id: 2,
+          title: "Medo",
+          show: false,
+          favorite: false
         },
         {
-          id: 5,
-          title: "Pirataria",
-          image: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-          src: "",
-          show: false
+          id: 1,
+          title: "O início",
+          show: false,
+          favorite: false
         }
-      ],
+      ]
     };
+  },
+  methods: {
+    playAudio: function(src) {
+      if (!this.playing) {
+        this.audio.src = src;
+        this.audio.play();
+        this.playing = true;
+      } else {
+        this.audio.pause();
+        this.playing = false;
+      }
+    }
   }
 };
 </script>
@@ -84,22 +94,25 @@ export default {
   padding-left: 5px;
 }
 
-.container-overlay{
-    position: relative;
+.container-overlay {
+  position: relative;
 }
 
-.overlay{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background: rgba(255, 85, 0, 0.86);   
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
+.overlay {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: rgba(255, 85, 0, 0.86);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 }
 
+.active {
+  color: #ed4956 !important;
+}
 </style>
